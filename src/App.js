72b7f58a118d790c7466cdc7213e1650dc56dda2
Leigh Robinson <leighspost@hotmail.com>
2023-02-22 21:33:18 +0000

    bulk of coding completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=5e1a88c0';
 
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 const App = () => {
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch (`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -28,7 +39,29 @@ const App = () => {
                   value="Superman"
                   onChange={() => {}}
                 />
+                <img 
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
             </div>
+
+            {
+                movies?.length > 0
+                  ? (
+                    <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                    </div>
+                  ) : (
+                    <div classname="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                  )
+
+            }
+
         </div>
     );
 }
